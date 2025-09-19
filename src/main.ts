2548fb4a2dd9adadae0f2bedd4b88b8c57ab6345
JoseErisvaldo <joseerisvaldopg@gmail.com>
2025-09-19 02:02:37 -0300

    feat: add enableCors

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,13 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('');
-  await app.listen(3000);
-  console.log('Server running on http://localhost:3000');
+  app.enableCors({
+    origin: 'https://front-sistema-react-vite.vercel.app', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  await app.listen(3001);
+  console.log('Server running on http://localhost:3001');
 }
 bootstrap();
