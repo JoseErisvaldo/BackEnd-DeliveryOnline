b34410b16728e4ddb9f43e80649e297fe8b7238d
JoseErisvaldo <joseerisvaldopg@gmail.com>
2025-09-21 15:50:19 -0300

    feat: add config data source prod & local

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,18 +1,20 @@
-// src/database/data-source.ts
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { User } from '../users/user.entity';
 import { Establishment } from '../establishment/entities/establishment.entity';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); // garante que o .env seja lido
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DATABASE_HOST || 'localhost',
-  port: Number(process.env.DATABASE_PORT) || 5432,
-  username: process.env.DATABASE_USER || 'postgres',
-  password: process.env.DATABASE_PASSWORD || 'postgres',
-  database: process.env.DATABASE_NAME || 'nestjs_auth',
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: String(process.env.DATABASE_PASSWORD),
+  database: process.env.DATABASE_NAME,
   entities: [User, Establishment],
   migrations: ['dist/migrations/*.js'],
   synchronize: false,
-  
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
