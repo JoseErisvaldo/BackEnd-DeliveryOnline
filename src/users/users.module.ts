b74ccb8722dde053af71e39ca7899a15bd599bb4
JoseErisvaldo <joseerisvaldopg@gmail.com>
2025-09-20 23:21:23 -0300

    fix: add establishment in users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,12 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { UsersService } from "./users.service";
-import { UsersController } from "./users.controller";
-import { User } from "./user.entity";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { User } from './user.entity';
+import { Establishment } from '../establishment/entities/establishment.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Establishment])],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],
